9145bc1c2e180a9a6232e9a27d0b0fcbd4ecbf76
Pedro Verdasca Cruz <peverdasca@gmail.com>
2026-05-11 08:04:29 -0700

    Add files via upload

---
+++ Python/Operadores_Aritmeticos/op_aritmeticos.py
@@ -0,0 +1,23 @@
+#variáveis
+a=13
+b=7
+
+#operadores
+soma=a+b
+subtração=a-b
+multiplicacao=a*b
+divisao=a/b
+div_inteira=a//b
+modulo=a%b
+potencia=a**b
+
+#resultados
+print(f"a ** b = {soma}")
+print(f"a + b = {subtração}")
+print(f"a - b = {multiplicacao}")
+print(f"a * b = {divisao}")
+print(f"a / b = {div_inteira}")
+print(f"a // b = {modulo}")
+print(f"a % b = {potencia}")
+
+
