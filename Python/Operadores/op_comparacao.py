71dc7d0a8630067a5b2153d802e884185d6059c3
Pedro Verdasca Cruz <peverdasca@gmail.com>
2026-05-11 13:54:58 -0700

    Update op_comparacao.py

--- Python/Operadores/op_comparacao.py
+++ Python/Operadores/op_comparacao.py
@@ -11,19 +11,48 @@ maior_igual=x>=y
 menor_igual=x<=y
 
 #resultados
-print(f"x > y = {maior_que}")
-print(f"x < y = {menor_que}")
-print(f"x == y = {igual_a}")
-print(f"x != y = {diferente_de}")
-print(f"x >= y = {maior_igual}")
-print(f"x <= y = {menor_igual}")
+print(f"a) x > y = {maior_que}")
+print(f"b) x < y = {menor_que}")
+print(f"c) x == y = {igual_a}")
+print(f"d) x != y = {diferente_de}")
+print(f"e) x >= y = {maior_igual}")
+print(f"f) x <= y = {menor_igual}")
 
-print( )
+print(-------------------------------------------------------)
 
 #v2 resultados
-print(f"{x} É maior que {y}? {maior_que}")
-print(f"{x} É menor que {y}? {menor_que}")
-print(f"{x} É igual a {y}? {igual_a}")
-print(f"{x} É diferente de {y}? {diferente_de}")
-print(f"{x} É maior ou igual a {y}? {maior_igual}")
-print(f"{x} É menor ou igual a {y}? {menor_igual}")
+#Maior que
+if maior_que == True:
+    print(f"a) {x} é maior que {y}? Sim") 
+else:
+    print(f"a) {x} é maior que {y}? Não") 
+
+#Menor que
+if menor_que == True:
+    print(f"b) {x} é menor que {y}? Sim") 
+else:
+    print(f"b) {x} é menor que {y}? Não") 
+
+#Igual a
+if igual_a == True:
+    print(f"c){x} é igual a {y}? Sim") 
+else:
+    print(f"c) {x} é igual a {y}? Não") 
+
+#Diferente de
+if diferente_de == True:
+    print(f"d) {x} é diferente de {y}? Sim") 
+else:
+    print(f"d) {x} é diferente de {y}? Não")
+
+#Maior ou igual
+if maior_igual == True:
+    print(f"e) {x} é maior ou igual a {y}? Sim") 
+else:
+    print(f"e) {x} é maior ou igual a {y}? Não")
+
+#Menor ou igual
+if menor_igual == True:
+    print(f"f) {x} é menor ou igual a {y}? Sim") 
+else:
+    print(f"f) {x} é menor ou igual a {y}? Não")
