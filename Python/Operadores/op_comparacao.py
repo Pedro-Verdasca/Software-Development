352b0e3728ecb2af9cfe5a2f8157cdad4983e2f9
Pedro Verdasca Cruz <peverdasca@gmail.com>
2026-05-11 13:24:50 -0700

    Add files via upload

---
+++ Python/Operadores/op_comparacao.py
@@ -0,0 +1,31 @@
+#variáveis
+x=3
+y=10
+
+#operadores
+maior_que=x>y
+menor_que=x<y
+igual_a=x==y
+diferente_de=x!=y
+maior_igual=x>=y
+menor_igual=x<=y
+
+print( )
+
+#resultados
+print(f"x > y = {maior_que}")
+print(f"x < y = {menor_que}")
+print(f"x == y = {igual_a}")
+print(f"x != y = {diferente_de}")
+print(f"x >= y = {maior_igual}")
+print(f"x <= y = {menor_igual}")
+
+print( )
+
+#v2 resultados
+print(f"{x} É maior que {y}? {maior_que}")
+print(f"{x} É menor que {y}? {menor_que}")
+print(f"{x} É igual a {y}? {igual_a}")
+print(f"{x} É diferente de {y}? {diferente_de}")
+print(f"{x} É maior ou igual a {y}? {maior_igual}")
+print(f"{x} É menor ou igual a {y}? {menor_igual}")
