1bb5ef165483051f2d83b7afdb19d47da12e40de
Pedro Verdasca Cruz <peverdasca@gmail.com>
2026-05-11 08:52:57 -0700

    Update op_aritmeticos.py

--- Python/Operadores_Aritmeticos/op_aritmeticos.py
+++ Python/Operadores_Aritmeticos/op_aritmeticos.py
@@ -1,23 +1,50 @@
 #variáveis
-a=13
-b=7
+a=10
+b=3
+c="8 "
+d="dias"
 
 #operadores
 soma=a+b
-subtração=a-b
+subtracao=a-b
 multiplicacao=a*b
 divisao=a/b
 div_inteira=a//b
 modulo=a%b
 potencia=a**b
 
+concate=c+d
+
+pot=a^b
+
+print( )
+
 #resultados
-print(f"a ** b = {soma}")
-print(f"a + b = {subtração}")
-print(f"a - b = {multiplicacao}")
-print(f"a * b = {divisao}")
-print(f"a / b = {div_inteira}")
-print(f"a // b = {modulo}")
-print(f"a % b = {potencia}")
+print(f"a + b = {soma}")
+print(f"a - b = {subtracao}")
+print(f"a * b = {multiplicacao}")
+print(f"a / b = {divisao:.2f}")
+print(f"a // b = {div_inteira}")
+print(f"a % b = {modulo}")
+print(f"a ** b = {potencia}")
 
+print( )
 
+#v2 resultados
+print(f"{a} + {b} = {soma}")
+print(f"{a} - {b} = {subtracao}")
+print(f"{a} * {b} = {multiplicacao}")
+print(f"{a} / {b} = {divisao:.3f}")
+print(f"{a} // {b} = {div_inteira}")
+print(f"{a} % {b} = {modulo}")
+print(f"{a} ** {b} = {potencia}")
+
+print( )
+
+#concatenação
+print(f"{c}+ {d} = {concate}")
+
+print( )
+
+#alternativa potenciação
+print(f"{a} ^ {b} = {pot}")
